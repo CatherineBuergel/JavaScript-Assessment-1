0fda0b16f4a10e47a7adee42706a4e482a90e3a5
Catherine Buergel <catherinebuergel@gmail.com>
2019-02-11 16:07:01 -0700

    numbers done

--- numbers.js
+++ numbers.js
@@ -2,10 +2,28 @@
 var num10 = 10;
 var string8 = '8';
 var one = 1;
-//2. write a loop that will log only numbers divisible by 3 between 20 - 100
 
+var sum = num10 + parseInt(string8) + one
+console.log(sum)
+//2. write a loop that will log only numbers divisible by 3 between 20 - 100
+function divisibleThree() {
+  for (i = 20; i < 101; i++) {
+    if (i % 3 == 0) {
+      console.log(i)
+    }
+  }
+}
 
 
 //3. find the class average given the following test scores
 
-var scores = [88, 84, 100, 92, 70, 76, 76, 84, 86, 98]; 
+var scores = [88, 84, 100, 92, 70, 76, 76, 84, 86, 98];
+
+function classAverage(arr) {
+  let total = 0
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i]
+  }
+  return total / arr.length
+
+}
